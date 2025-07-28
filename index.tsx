07aca4812665609f8230fa88f3c6f074934e9aaf
Max0115 <91790311+Max0115@users.noreply.github.com>
2025-07-28 14:57:17 +0800

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,7 +1,3 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { createRoot } from "react-dom/client";
 
